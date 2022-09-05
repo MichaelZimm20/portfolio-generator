e162c1e5b0b352ef267582a399ec891405f80540
DJMIKE20 <m.zimm20@gmail.com>
2022-09-05 13:17:04 -0400

    installed inquirer with npm and added gitignore and created question prompt

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
 
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
 
 // The first argument is the file name that will be created, or the output file. 
 // The second argument is the data that's being written: the HTML string template. 
 // The third argument is the callback function that will handle any errors as well as the success message.
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', err => {
+//     if (err) throw err;
 
-    console.log('Portofolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portofolio complete! Check out index.html to see the output!');
+// });
 
 
 
